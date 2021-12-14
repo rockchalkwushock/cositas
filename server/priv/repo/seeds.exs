39ace367841650ee086b33c183a0d6c66d25ea04
Cody Brunner <me@codybrunner.com>
2021-12-14 10:42:28 -0500

    feat(server): adds seeds

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -9,3 +9,31 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias Faker
+alias App.Repo
+alias App.Accounts.User
+alias App.Content.Project
+
+# Create a user
+turd =
+  %User{}
+  |> User.changeset(%{
+    email: "turd@gmail.com",
+    first_name: "Turd",
+    last_name: "Ferguson",
+    password: "hello-World123**",
+    username: "da1RealTurd"
+  })
+  |> Repo.insert!()
+
+for _ <- 1..10 do
+  %Project{
+    end_date: Faker.DateTime.between(
+          DateTime.to_naive(DateTime.utc_now()),
+          DateTime.to_naive(Faker.DateTime.forward(30))
+        ),
+    owner_id: turd.id,
+    start_date: DateTime.utc_now(),
+    title: Faker.Lorem.word()
+  } |> Repo.insert!()
+end
