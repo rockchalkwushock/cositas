2342a71327d8fbbec0326c07efcb9e910c807fed
Cody Brunner <me@codybrunner.com>
2021-12-15 10:17:20 -0500

    feat(client): adds date-fns

--- client/utils/helpers.ts
+++ client/utils/helpers.ts
@@ -1,1 +1,26 @@
+import { format } from 'date-fns'
+
 export const isServer = typeof window === 'undefined'
+
+type Formats =
+  | 'full-localized'
+  | 'short-localized'
+  | 'short-time-with-date-localized'
+  | 'time-localized'
+
+const formatsMap = {
+  'full-localized': 'PPPP',
+  'short-localized': 'PP',
+  'short-time-with-date-localized': 'P ppp',
+  'time-localized': 'ppp',
+}
+
+export const formatDateTime = (
+  dateTimeString: string,
+  presentation?: Formats
+) => {
+  return format(
+    new Date(dateTimeString),
+    formatsMap[presentation || 'full-localized']
+  )
+}
