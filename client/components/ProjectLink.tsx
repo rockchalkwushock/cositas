11a82c6c798af28e793b88c2b35f72f4bc6d4d9b
Cody Brunner <me@codybrunner.com>
2021-12-15 10:17:34 -0500

    feat(client): adds ProjectLink

---
+++ client/components/ProjectLink.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+type ProjectLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  id: string
+}
+
+// eslint-disable-next-line react/display-name
+export const ProjectLink = React.forwardRef<
+  HTMLAnchorElement,
+  ProjectLinkProps
+>(({ children, id, onClick, ...rest }, ref) => {
+  const { query } = useRouter()
+  console.log('ProjectLink', query, `/u/${query.userId}/p/${id}/`)
+  return (
+    <Link href={`/u/${query.userId}/p/${id}/`} passHref>
+      <a {...rest} onClick={onClick} ref={ref}>
+        {children}
+      </a>
+    </Link>
+  )
+})
